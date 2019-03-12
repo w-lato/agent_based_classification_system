d7e72e9e108abb1f11bf8827a4756d5298ee213e
w-lato <w.latosinski@gmail.com>
2019-03-12 12:56:42 +0100

    Splitter with equal and OL methods with tests

--- src/main/java/agh/edu/agents/Splitter.java
+++ src/main/java/agh/edu/agents/Splitter.java
@@ -2,11 +2,19 @@ package agh.edu.agents;
 
 import agh.edu.learning.DataSplitter;
 import akka.actor.AbstractActor;
+import akka.actor.ActorSystem;
+import org.opencv.core.Mat;
+import weka.core.Instance;
 import weka.core.Instances;
-import java.util.List;
+import weka.core.converters.ConverterUtils;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
 
 // TODO stratify the data when splitting
-public class Splitter extends AbstractActor
+public class Splitter// extends AbstractActor
 {
     private List<Instances> train;
     private Instances eval;
@@ -22,6 +30,38 @@ public class Splitter extends AbstractActor
         //
     }
 
+
+    public static List<Instances> OLsplit(Instances data, double OL, int n) throws IOException {
+       data.stratify( n );
+       List<Instances> l = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            l.add( data.testCV( n, i) );
+        }
+        int x_split = (int) (1.0 / OL);
+        for (int i = 1; i < n; i++) {
+            Instances prev = l.get( i - 1 );
+            prev.stratify( x_split );
+            l.get( i ).addAll( prev.testCV( x_split,0 ) );
+//            System.out.println( l.get(i).size() );
+        }
+        return l;
+//        String s1 = l.get(0).toString();
+//        Files.write(Paths.get("DATA/a.txt"), s1.getBytes());
+//        String s2 = l.get(1).toString();
+//        Files.write(Paths.get("DATA/b.txt"), s2.getBytes());
+//        System.out.println( data.size() );
+    }
+
+    public static List<Instances> equalSplit(Instances data, int n) throws IOException {
+        data.stratify( n );
+        List<Instances> l = new ArrayList<>();
+        for (int i = 0; i < n; i++)
+        {
+            l.add( data.testCV( n, i) );
+        }
+        return l;
+    }
+
     ///////////////////////////////////////////////////////////////////////////////////////////
     //
     //
@@ -30,13 +70,22 @@ public class Splitter extends AbstractActor
 
 
 
-    @Override
-    public Receive createReceive()
-    {
-        return receiveBuilder()
-                .match(  RunConf.class, this::onSplit)
-                .matchAny(o -> { System.out.println("Master received unknown message: " + o); })
-                .build();
-    }
+//    @Override
+//    public Receive createReceive()
+//    {
+//        return receiveBuilder()
+//                .match(  RunConf.class, this::onSplit)
+//                .matchAny(o -> { System.out.println("Master received unknown message: " + o); })
+//                .build();
+//    }
 
+    public static void main(String[] args) throws Exception {
+        ConverterUtils.DataSource source = new ConverterUtils.DataSource( "DATA\\mnist_train.arff");
+        Instances data = source.getDataSet();
+        data.randomize( new Random(1));
+        data.setClassIndex( data.numAttributes() - 1);
+
+        Splitter s = new Splitter();;
+        OLsplit( data.testCV(10,0), 0.15, 10 );
+    }
 }
---
+++ src/test/java/agents/SplitterTest.java
@@ -0,0 +1,60 @@
+package agents;
+
+import agh.edu.agents.Splitter;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import weka.core.Instances;
+import weka.core.converters.ConverterUtils;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.stream.Collectors;
+
+public class SplitterTest
+{
+    static int N = 50;
+    static Instances data;
+
+
+    @BeforeClass
+    public static void setup() throws Exception {
+        ConverterUtils.DataSource source = new ConverterUtils.DataSource( "DATA\\mnist_train.arff");
+        data = source.getDataSet();
+        data.randomize( new Random(1));
+        data.setClassIndex( data.numAttributes() - 1);
+        data.stratify( N );
+    }
+
+    @Test
+    public void testEqualSplit() throws IOException {
+        int n = 10;
+        List<Instances> l = Splitter.equalSplit( data, n );
+        for (int i = 1; i < n; i++)
+        {
+            List<String> s1 = Arrays.stream(l.get(i - 1).toString().split("\n")).filter(x -> !(x.startsWith("@") || x.isEmpty())).collect(Collectors.toList());
+            List<String> s2 = Arrays.stream(l.get(i).toString().split("\n")).filter(x -> !(x.startsWith("@") || x.isEmpty())).collect(Collectors.toList());
+            s2.retainAll( s1 );
+            Assert.assertTrue( s2.isEmpty()  );
+        }
+    }
+
+    @Test
+    public void testOLSplit() throws IOException {
+        int n = 10;
+        double OL = 0.05;
+        List<Instances> l = Splitter.OLsplit( data, OL, n );
+        for (int i = 1; i < n; i++)
+        {
+            List<String> s1 = Arrays.stream(l.get(i - 1).toString().split("\n")).filter(x -> !(x.startsWith("@") || x.isEmpty())).collect(Collectors.toList());
+            List<String> s2 = Arrays.stream(l.get(i).toString().split("\n")).filter(x -> !(x.startsWith("@") || x.isEmpty())).collect(Collectors.toList());
+            int orig_siz = s2.size();
+            s2.retainAll( s1 );
+//            System.out.println( orig_siz + " : " + s2.size() );
+            Assert.assertTrue( orig_siz >= (OL * s1.size())  );
+        }
+    }
+
+}
